585f0877626b6589e07bab16dac8d2ceedbfbb1d
Roman <74456584+hortn@users.noreply.github.com>
2023-09-14 12:06:13 +0600

    Add files via upload

---
+++ get_account_state.ps1
@@ -0,0 +1,33 @@
+﻿function get_acc_state {
+param ([array]
+[ValidateNotNullorEmpty()]
+[parameter(mandatory = $true)]
+$user
+)
+
+$acc_info = $user |Get-ADUser -Properties * | Select-Object Enabled, LockedOut, SamAccountName
+
+foreach ($temp in $acc_info){
+
+if ($temp.Enabled -ne $true){
+[array]$info += "$($temp.SamAccountName) -disabled"
+}
+
+elseif ($temp.LockedOut){
+[array]$info += "$($temp.SamAccountName) -locked"
+}
+
+else{
+[array]$info += "$($temp.SamAccountName) -OK"
+}
+}
+return $info
+}
+
+
+
+
+#example
+get_acc_state user1, user2, user3, user...n
+
+#or use array in first param
